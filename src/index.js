7a21727e01c604d6a9de41d87b0310fdfd57e133
m0i5 <111139099+m0i5@users.noreply.github.com>
2023-04-14 17:35:55 +0200

    Imports

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { useLayoutEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+
+const root = createRoot (document.getElementById("root"));
+
+root.render(
+  <ul>
+    <li>
+      Swanek
+    </li>
+  </ul>
+)
+
